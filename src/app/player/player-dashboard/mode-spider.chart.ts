9d1fc63dd5a5784c42d867de760e168842e34935
R.Zdulski <r.p.zdulski@gmail.com>
2022-08-18 23:52:33 +0200

    added radar charts and mode cards to player-dashboard.component

---
+++ src/app/player/player-dashboard/mode-spider.chart.ts
@@ -0,0 +1,97 @@
+import {baseStats} from "../../../dtos/player-mode-stats";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts"
+
+const minWr = 0.35
+const maxWr = 0.70
+const minKdr = 0.5
+const maxKdr = 3.5
+const minKsr = 0.5
+const maxKsr = 3.5
+const minBattles = 10000
+const maxBattles = 50000
+const minSurviv = 0.10
+const maxSurviv = 0.90
+
+// normalizing value to a number between 0 and 1
+function normalizeValue(value: number, min: number, max: number):number {
+  let temp: number = (value - min) / (max - min);
+  if (temp < 0) return 0;
+  if (temp > 1) return 1;
+  else return temp;
+}
+
+// using amcharts4 because there is no option for polygonal gridType in amcharts 5
+export function initModeSpiderChart4(modeStats: baseStats, id: string, color: string){
+  /* Create chart instance */
+  let chart = am4core.create(id, am4charts.RadarChart);
+  chart.align = "center";
+  chart.layout = "horizontal";
+  chart.radius = am4core.percent(90);
+
+  /* Add data */
+  chart.data = [{
+    "category": "WR[%]",
+    "value": (modeStats.wins/modeStats.battles*100).toFixed(2)+"%",
+    "normalizedValue": normalizeValue((modeStats.wins/modeStats.battles), minWr, maxWr),
+    "name": "win ratio"
+  }, {
+    "category": "K/D",
+    "value": ((modeStats.air_kills+modeStats.ground_kills)/modeStats.deaths).toFixed(2),
+    "normalizedValue": normalizeValue((modeStats.air_kills+modeStats.ground_kills)/modeStats.deaths, minKdr, maxKdr),
+    "name": "kills/death"
+  }, {
+    "category": "K/S",
+    "value": ((modeStats.air_kills+modeStats.ground_kills)/modeStats.spawns).toFixed(2),
+    "normalizedValue": normalizeValue((modeStats.air_kills+modeStats.ground_kills)/modeStats.spawns, minKsr, maxKsr),
+    "name": "kills/spawn"
+  }, {
+    "category": "Battles",
+    "value": modeStats.battles,
+    "normalizedValue": normalizeValue(modeStats.battles, minBattles, maxBattles),
+    "name": "battles"
+  }, {
+    "category": "Surviv.",
+    "value": ((1-modeStats.deaths/modeStats.spawns)*100).toFixed(2)+"%",
+    "normalizedValue": normalizeValue(1-modeStats.deaths/modeStats.spawns, minSurviv, maxSurviv),
+    "name": "survivability"
+  }];
+
+  /* Create axes */
+  // @ts-ignore
+  let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+  // @ts-ignore
+  categoryAxis.dataFields.category = "category";
+  categoryAxis.fill = chart.colors.getIndex(2);
+  // @ts-ignore
+  categoryAxis.tooltip.disabled = true;
+  // categoryAxis.renderer.radius = am4core.percent(65);
+  categoryAxis.renderer.labels.template.radius = am4core.percent(-1)
+  categoryAxis.renderer.labels.template.textAlign = "middle"
+  categoryAxis.zIndex = 10;
+
+  // @ts-ignore
+  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.renderer.labels.template.hidden = true;
+  valueAxis.renderer.gridType = "polygons";
+  // @ts-ignore
+  valueAxis.min = 0;
+  // @ts-ignore
+  valueAxis.max = 1;
+
+  /* Create and configure series */
+  let series = chart.series.push(new am4charts.RadarSeries());
+  series.dataFields.valueY = "normalizedValue";
+  series.dataFields.categoryX = "category";
+  series.strokeWidth = 2;
+  series.stroke = am4core.color(color);
+  series.fillOpacity = 0.4;
+  series.tooltipText = "{name}:\n{value}";
+  series.fill = am4core.color(color);
+
+  /* Create and configure cursor */
+  chart.cursor = new am4charts.RadarCursor();
+  chart.cursor.lineX.hidden = true;
+  chart.cursor.lineY.hidden = true;
+
+}
--- src/app/player/player-dashboard/player-dashboard.component.ts
+++ src/app/player/player-dashboard/player-dashboard.component.ts
@@ -1,17 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, RendererFactory2} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PlayerService} from "../../../services/player.service";
-import {PlayerModeStats} from "../../../dtos/player-mode-stats";
+import {baseStats, PlayerModeStats} from "../../../dtos/player-mode-stats";
 import {initBattleDonutChart} from "./battle-donut.chart";
+import {initModeSpiderChart4} from "./mode-spider.chart";
+import {mode_colors} from "./color.scheme";
+
+// used to allow the use of *ngFor directive
+interface dto{
+  mode_id: string;
+  mode_name: string;
+  mode_stats: baseStats;
+};
 
 @Component({
   selector: 'app-player-dashboard',
   templateUrl: './player-dashboard.component.html',
   styleUrls: ['./player-dashboard.component.css']
 })
-export class PlayerDashboardComponent implements OnInit {
+export class PlayerDashboardComponent implements OnInit, AfterViewInit {
+  [x: string]: any;
   login!: string;
-  public playerStats!: PlayerModeStats;
+  playerStats!: PlayerModeStats;
+  modeStats!: dto[];
+  showKillsRatios = [false, false, false, false, false, false];
+  showSpawnsRatios = [false, false, false, false, false, false];
+  showKills = [false, false, false, false, false, false];
+  showWins = [false, false, false, false, false, false];
+  showBattles = [false, false, false, false, false, false];
 
   constructor(private route: ActivatedRoute,
               private playerService: PlayerService) {
@@ -21,17 +37,50 @@ export class PlayerDashboardComponent implements OnInit {
       if (this.login == undefined)
         throw new Error("login cannot be empty");
     });
+  }
 
+  ngAfterViewInit(): void {
+    initModeSpiderChart4(this.playerStats.air_ab, "air_ab-radar-chart", mode_colors.arcade);
+    initModeSpiderChart4(this.playerStats.ground_ab, "ground_ab-radar-chart", mode_colors.arcade);
+    initModeSpiderChart4(this.playerStats.air_rb, "air_rb-radar-chart", mode_colors.realistic);
+    initModeSpiderChart4(this.playerStats.ground_rb, "ground_rb-radar-chart", mode_colors.realistic);
+    initModeSpiderChart4(this.playerStats.air_sb, "air_sb-radar-chart", mode_colors.simulation);
+    initModeSpiderChart4(this.playerStats.ground_sb, "ground_sb-radar-chart", mode_colors.simulation);
+  }
+
+  ngOnInit(): void {
     this.playerService.getPlayerModesStats(this.login).subscribe(
       data => {
         this.playerStats = data;
         initBattleDonutChart(data, "battle-donut-chart");
+
+        this.modeStats = [{
+          mode_id: "air_ab",
+          mode_name: "Air Arcade",
+          mode_stats : this.playerStats.air_ab,
+        },{
+          mode_id: "ground_ab",
+          mode_name: "Ground Arcade",
+          mode_stats : this.playerStats.ground_ab,
+        },{
+          mode_id: "air_rb",
+          mode_name: "Air  Realistic",
+          mode_stats : this.playerStats.air_rb,
+        },{
+          mode_id: "ground_rb",
+          mode_name: "Ground Realistic",
+          mode_stats : this.playerStats.ground_rb,
+        },{
+          mode_id: "air_sb",
+          mode_name: "Air Simulation",
+          mode_stats : this.playerStats.air_sb,
+        },{
+          mode_id: "ground_sb",
+          mode_name: "Ground Simulation",
+          mode_stats : this.playerStats.ground_sb,
+        }]
+
       });
-  }
-
-  ngOnInit(): void {
-
-
   }
 }
 
